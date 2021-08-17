3c8eb7efcd72fe496503b7162705483bcc4a4c92
sobriver <sobriver@163.com>
2021-08-17 15:27:58 +0800

    233

---
+++ multimedia/video_to_picture.py
@@ -0,0 +1,49 @@
+import cv2
+
+"""
+将视频按照帧率截取成一张张的图片
+"""
+
+# 视频地址
+VIDEO_PATH = r'D:\tmp\video\1920x1080.264'
+# 存放帧图片的位置
+EXTRACT_FOLDER = r'D:\tmp\video\photo'
+# 帧提取频率
+EXTRACT_FREQUENCY = 50
+
+
+def extract_frames(video_path, dst_folder, index):
+    video = cv2.VideoCapture()
+    if not video.open(video_path):
+        print("can not open the video")
+        exit(1)
+    count = 1
+    while True:
+        _, frame = video.read()
+        if frame is None:
+            break
+        if count % EXTRACT_FREQUENCY == 0:
+            save_path = "{}/{:>03d}.jpg".format(dst_folder, index)
+            cv2.imwrite(save_path, frame)
+            print('save path:' + str(save_path))
+            index += 1
+        count += 1
+    video.release()
+    # 打印出所提取帧的总数
+    print("Totally save {:d} pics".format(index-1))
+
+
+
+if __name__ == '__main__':
+    # 递归删除之前存放帧图片的文件夹，并新建一个
+    import shutil
+
+    try:
+        shutil.rmtree(EXTRACT_FOLDER)
+    except OSError:
+        pass
+    import os
+
+    os.mkdir(EXTRACT_FOLDER)
+    # 抽取帧图片，并保存到指定路径
+    extract_frames(VIDEO_PATH, EXTRACT_FOLDER, 1)
